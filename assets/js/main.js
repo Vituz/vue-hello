27597567100415c12db5af4440de2973014b22b1
Vituz <vtbattaglia@gmail.com>
2021-05-17 16:30:50 +0200

    Complete

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/*
+Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const app = new Vue({
+
+    el: '#root',
+
+    data:{
+        message: 'Benvenuto in Vue-Js',
+        welcomeImg: './assets/img/vue-js.png'
+    }
+
+});
